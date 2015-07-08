1b068d3478bf34205db790ce58df89614a23d71c
sergelus <sergelus@yandex.ru>
2015-07-08 17:22:45 +0300

    v 0.05

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -3,14 +3,15 @@ class CarsController < ApplicationController
     @cars = Car.all
     i = 0;
     j = 0;
-    @buz = [];
+    @buz = []
     @cars.each do |ca|
       ca.contracts.each do |co|
-      @buz[i] = co.order_date.to_s[5..6].to_i 
-      @buz[i] = co.order_date.to_s[5..6].to_i
+      @buz[i] = j.to_s + co.order_date.to_s[5..6].to_s 
       i = i + 1  
     end
+      j = j + 1
     end
+    @buz = [3,4,5, 10,11,12,23,25,28]
   end
   
   def show    
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,10 +3,10 @@ class WelcomeController < ApplicationController
     @cars = Car.all
     i = 0;
     j = 0;
+    @buz = []
      @cars.each do |ca|
       ca.contracts.each do |co|
       @buz[i] = co.order_date.to_s[5..6].to_i 
-      @buz[i] = co.order_date.to_s[5..6].to_i
       i = i + 1  
     end
     end
